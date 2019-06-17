70cad0024ecca06a96a68a298179f66291b9f5ea
Vincent Honca <vincenth1985@gmail.com>
2019-06-17 11:14:05 +0200

    service package and uml forestbook done

---
+++ src/entities/services/ForestNotebook.java
@@ -0,0 +1,77 @@
+package entities.services;
+
+import entities.animal_entities.Animal;
+import entities.animal_entities.Carnivore;
+import entities.animal_entities.Herbivore;
+import entities.animal_entities.Omnivore;
+import entities.plant_entities.Plant;
+
+import java.util.List;
+
+public class ForestNotebook {
+
+    private List<Carnivore> carnivores;
+    private List<Omnivore> omnivores;
+    private List<Herbivore> herbivores;
+    private int plantCount;
+    private int animalCount;
+    private List<Animal> animals;
+    private List<Plant> plants;
+
+    public ForestNotebook() {
+    }
+
+    public List<Carnivore> getCarnivores() {
+        return carnivores;
+    }
+
+    public void setCarnivores(List<Carnivore> carnivores) {
+        this.carnivores = carnivores;
+    }
+
+    public List<Omnivore> getOmnivores() {
+        return omnivores;
+    }
+
+    public void setOmnivores(List<Omnivore> omnivores) {
+        this.omnivores = omnivores;
+    }
+
+    public List<Herbivore> getHerbivores() {
+        return herbivores;
+    }
+
+    public void setHerbivores(List<Herbivore> herbivores) {
+        this.herbivores = herbivores;
+    }
+
+    public int getPlantCount() {
+        return plantCount;
+    }
+
+    public int getAnimalCount() {
+        return animalCount;
+    }
+
+    public void addAnimal(Animal animal) {
+
+    }
+
+    public void addPlant(Plant plant){
+
+    }
+
+    public void printNotebook(){
+
+    }
+
+    public void sortAnimalsByName(){
+
+    }
+    
+    public void sortPlantsByName(){
+
+    }
+
+
+}
